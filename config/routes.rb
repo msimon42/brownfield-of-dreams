694b2172107f2e5980dfca7a478c0d2a2f4c4347
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-11 15:56:02 -0700

    Add route to user activation page
    Co-Authored-By: null <msimon42@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,9 @@ Rails.application.routes.draw do
   # Is this being used?
   get '/video', to: 'video#show'
 
-  resources :users, only: [:new, :create, :update, :edit]
+  resources :users, only: [:new, :create, :update, :edit] do
+    get '/activate', to: 'activation#update'
+  end
 
   resources :tutorials, only: [:show, :index] do
     resources :videos, only: [:show, :index]

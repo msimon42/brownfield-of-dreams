fe6a1291a092a1988636f547f92eb20bdd69ed2f
msimon42 <msimon@uab.edu>
2020-02-11 22:29:18 -0700

    Refactor controller to filter tutorials

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-    else
-      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    end
+    @tutorials = filter_tutorials
   end
+
+  private
+
+    def filter_tutorials
+      case current_user
+      when nil
+        return Tutorial.non_classroom.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5) if params[:tag]
+        return Tutorial.non_classroom.paginate(page: params[:page], per_page: 5)
+      else
+        return Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5) if params[:tag]
+        return Tutorial.all.paginate(page: params[:page], per_page: 5)
+      end
+    end
 end

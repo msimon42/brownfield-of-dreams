afbaaf2b88722f976bd33ae4ac8e3b3f922dfae4
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-12 16:38:37 -0700

    Add ordered scope to video model for use on user dashboard

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,6 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  scope :ordered, -> { order(:tutorial_id, :position) }
 end

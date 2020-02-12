79f5b0d57ba1d0010f19443a8847b33ba6ce92af
msimon42 <msimon@uab.edu>
2020-02-11 22:29:01 -0700

    Add test for non_tutorial

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it 'non_classroom' do
+      @tutorial_1 = create :tutorial
+      @tutorial_4 = create :tutorial
+      @tutorial_2 = create :tutorial, classroom: true
+      @tutorial_3 = create :tutorial, classroom: true
+
+      expect(Tutorial.non_classroom).to eq([@tutorial_2, @tutorial_3])
+    end
+  end
 end

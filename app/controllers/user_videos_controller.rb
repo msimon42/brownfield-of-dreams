864e495280500a76825e61044083b1c17e6df2a5
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-12 21:36:11 -0700

    Add conditional sad path messaging to bookmark creation action

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -2,13 +2,16 @@ class UserVideosController < ApplicationController
   def new; end
 
   def create
-    user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-      flash[:error] = 'Already in your bookmarks'
-    elsif user_video.save
-      flash[:success] = 'Bookmark added to your dashboard!'
+    if params[:user_id] == 'visitor'
+      flash[:error] = "You must be logged in to bookmark videos."
+    else
+      user_video = UserVideo.new(user_video_params)
+      if current_user.user_videos.find_by(video_id: user_video.video_id)
+        flash[:error] = 'Already in your bookmarks'
+      elsif user_video.save
+        flash[:success] = 'Bookmark added to your dashboard!'
+      end
     end
-
     redirect_back(fallback_location: root_path)
   end
 

08be2e0bcc0eabaeb56c3740ed65f52a614e7dce
msimon42 <msimon@uab.edu>
2020-02-13 11:42:47 -0700

    Remove useless code

--- app/controllers/admin/api/v1/base_controller.rb
+++ app/controllers/admin/api/v1/base_controller.rb
@@ -5,14 +5,6 @@ class Admin::Api::V1::BaseController < ActionController::API
     four_oh_four unless current_user.admin?
   end
 
-  def current_user
-    @current_user ||= if session[:user_id]
-                        User.find(session[:user_id])
-                      else
-                        User.new
-                      end
-  end
-
   def four_oh_four
     raise ActionController::RoutingError, 'Not Found'
   end
